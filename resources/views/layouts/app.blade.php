50a5c637361723f73df09211b57ada5e1c8a62b6
Max <linme00p@gmail.com>
2021-03-18 18:14:53 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <section class="px-8">
+        <header>
+            <h1>Tweety</h1>
+        </header>
+    </section>
+    <section class="px-8">
+        <main>
+            @yield('content')
+        </main>
+    </section>
+</div>
+</body>
+</html>
