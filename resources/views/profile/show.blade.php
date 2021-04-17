8356dc7f2b86623ed5c94674f867c3861f841f43
Max <linme00p@gmail.com>
2021-04-17 17:31:44 +0300

    Fix size banner bug on mobile version

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,7 +1,9 @@
 <x-app>
     <header class="mb-6">
         <div class="banner relative flex justify-center mb-2">
-            <img src="{{ $user->banner }}" alt="Banner" class="max-h-96 rounded-3xl">
+            <figure>
+                <img src="{{ $user->banner }}" alt="Banner" width="100%" height="100%" class="h-auto md:max-h96 rounded-3xl">
+            </figure>
             <img src="{{ $user->avatar }}"
                  class="rounded-full absolute top-full left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt=""
                  style="width: 100px">
@@ -15,7 +17,8 @@
 
             <div>
                 @can('edit', $user)
-                    <a href="{{ route('profile.edit', $user->username) }}" class="rounded-full border border-gray-300 py-2 px-4 mr-1 text-sm mb-1 block float-left">
+                    <a href="{{ route('profile.edit', $user->username) }}"
+                       class="rounded-full border border-gray-300 py-2 px-4 mr-1 text-sm mb-1 block float-left">
                         Edit Profile
                     </a>
                 @endcan
